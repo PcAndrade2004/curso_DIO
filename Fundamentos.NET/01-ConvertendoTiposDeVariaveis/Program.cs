c80f9a919baa727b43c6ea81eac4d29915e2bedb
Paulo <pcandradeoliveira@hotmail.com>
2025-06-30 11:10:15 -0300

    first commit

---
+++ Fundamentos.NET/01-ConvertendoTiposDeVariaveis/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("## Convertendo Tipos de Variáveis ##\n");
+
+// Casting -> usando o Convert
+int a = Convert.ToInt32("5");   // Converte a string "5" para int
+Console.WriteLine(a);          // Saída: 5
+
+double b = double.Parse("4");  // Converte a string "4" para double
+Console.WriteLine(b);          // Saída: 4
+
+Console.ReadKey();
---
+++ Fundamentos.NET/02-ConversaoParaString/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("## Conversão para o Tipo String ##\n");
+
+//forma errada 
+//string a = 5;
+
+//forma correta :
+string a = Convert.ToString("5");
+Console.WriteLine(a);
+
+//quando queremos converter algum tipo para string devemos utilizar o ToString();
+// não podemos utilizar nenhum outro metodo, pois não existem alem do ToString();
+
+
+Console.ReadKey();
+
