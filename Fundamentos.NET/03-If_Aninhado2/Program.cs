7200eced00e217b58a486caf103bf01b60ef69be
Paulo <pcandradeoliveira@hotmail.com>
2025-06-30 15:03:07 -0300

    Estudo sobre if aninhado, utilizando o else-if

---
+++ Fundamentos.NET/03-If_Aninhado2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("## If aninhados ##\n");
+
+//If aninhado : Quando você tem varias condições para verificar, você pode usar o if aninhado.
+
+int quantidadeEstoque = 30;
+int quantidadeCompra = 50;
+bool possivelCompra = quantidadeCompra > 0 && quantidadeEstoque >= quantidadeCompra;//as duas condições devem ser verdadeiras para que a compra seja possivel
+
+Console.WriteLine($"É possivel realizar a venda? {possivelCompra} ");
+
+if (quantidadeCompra == 0)
+{
+    Console.WriteLine("A quantidade não pode ser menor que zero");
+}
+else if (possivelCompra)
+{
+    Console.WriteLine("Venda Realizada!");
+}else
+{
+    Console.WriteLine("Compra invalida");
+}
