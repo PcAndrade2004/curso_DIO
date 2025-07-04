3c7117929b825aa5d54597f6fca305d4ef9d87e5
PauloCesar <pcandradeoliveira@hotmail.com>
2025-07-04 20:18:06 -0300

    Operadores aritimeticos

---
+++ Fundamentos.NET/06-OperadoresAritimeticos/Calculadora.cs
@@ -0,0 +1,41 @@
+﻿namespace OperadoresAritimeticos
+{
+    // Classe Calculadora que contém os métodos para as operações
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"A soma de: {x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"A subtração de: {x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"A multiplicação de: {x} * {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+                Console.WriteLine($"A divisão de: {x} / {y} = {x / y}");
+          
+        }
+
+        public void Potencia(int x, int y)
+        {
+            //elevado devemos usar o ^ 
+            double pot = Math.Pow(x, y);//Pow -> significa a potencia
+            Console.WriteLine($"A potencia de: {x}^{y} = {pot}");
+        }
+        
+        public void raizQuadrada(double x)
+        {
+            //sqrt => Raiz Quadrada Calculo.
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz quadrada de {x} é: =  {Math.Sqrt(raiz)}");
+        }
+    }
+}
--- Fundamentos.NET/06-OperadoresAritimeticos/Principal.cs
+++ Fundamentos.NET/06-OperadoresAritimeticos/Principal.cs
@@ -1,6 +1,20 @@
-﻿namespace _06_OperadoresAritimeticos;
+﻿using OperadoresAritimeticos;
+
+namespace _06_OperadoresAritimeticos;
 
 public class Principal
 {
-    
+    public static void Main(string[] args)
+    {
+        Calculadora calc = new Calculadora();
+        //quando estamos passando parâmetros colocamos a virgula para delimitar.
+        calc.Somar(30, 30);
+        calc.Subtrair(40, 20);
+        calc.Multiplicar(30, 30);
+        calc.Dividir(30, 30);   
+        calc.Potencia(20, 30);
+        
+        
+        Console.ReadKey();
+    }
 }
